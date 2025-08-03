c4443e29e2b3376906d0ac932b91478d51cad74d
sed000 <sedmirhandoncu@gmail.com>
2025-08-03 20:26:49 +0300

    app name fix

--- config/preview.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_live_Y2xlcmsuc29yemFrLmNvbSQ",
-  EXPO_PUBLIC_AZURE_URL: "https://podcast-cmgng5g9gqe5g9h2.polandcentral-01.azurewebsites.net/api/gemini?code=f8OQpFCWiNiU3gyJNfDWvSkU4NFwh1NSf-IqPp6ylWr8AzFu506kRg==",
-  EXPO_PUBLIC_REVENUECAT_PROJECT_GOOGLE_API_KEY: "goog_xQRpbbDnFMtjCFmwBHVdrbKZWbu",
-  EXPO_PUBLIC_CONVEX_URL: "https://greedy-tortoise-466.convex.cloud"
-};
