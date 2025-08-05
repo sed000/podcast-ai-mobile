d630fff154ea8b3309698c2600770579f50b5a2a
sed000 <sedmirhandoncu@gmail.com>
2025-08-05 20:46:00 +0300

    dotenv setup

---
+++ types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+    export const EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+    export const EXPO_PUBLIC_AZURE_URL: string;
+    export const EXPO_PUBLIC_REVENUECAT_PROJECT_GOOGLE_API_KEY: string;
+    export const EXPO_PUBLIC_CONVEX_URL: string;
+  }
